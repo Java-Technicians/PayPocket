543e3daf70af664eb086c9f92c74c19620c776a6
Karen Lettieri <karulettieri2109@gmail.com>
2023-07-10 12:12:15 -0300

    first commit

---
+++ src/main/java/com/alkemy/paypocket/PaypocketApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.paypocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypocketApplication.class, args);
+	}
+
+}
