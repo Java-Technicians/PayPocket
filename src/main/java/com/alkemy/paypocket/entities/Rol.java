136ee1395647b5257a4424c13fdb0ef977179dff
ignacioxherrera <ignacioherrera20212@gmail.com>
2023-07-13 11:28:48 -0300

    Agregada la entitad roles

---
+++ src/main/java/com/alkemy/paypocket/entities/Rol.java
@@ -0,0 +1,33 @@
+package com.alkemy.paypocket.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Setter
+@Getter
+@ToString
+@Entity
+public class Rol {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int idcliente;
+
+    @Column(name="rolName")
+    private String name;
+
+    @Column(name="rolDesc")
+    private String descripcion;
+
+    @Column(name = "creationDate")
+    private Timestamp creationDate;
+
+    @Column(name = "updateDate")
+    private Timestamp updateDate;
+
+
+}
