15c70b4299a152e7a9a357e57307fb18dc2ba38a
ignacioxherrera <ignacioherrera20212@gmail.com>
2023-07-17 19:28:18 -0300

    Añadido el nullable

--- src/main/java/com/alkemy/paypocket/entities/Role.java
+++ src/main/java/com/alkemy/paypocket/entities/Role.java
@@ -17,28 +17,27 @@ import java.sql.Timestamp;
 @Table(name = "roles")
 public class Role {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="role_id", length = 100, nullable = false)
+    private int role_id;
 
-    @Column(name="role_name")
+    @Column(name="role_name", length = 100, nullable = false)
     private String name;
 
-    @Column(name="role_desc")
+    @Column(name="role_desc", length = 100, nullable = false)
     private String descripcion;
 
-    /*
-    @Column(name = "creationDate")
-   private Timestamp creationDate;
+    @Column(name = "creationDate", length = 100, nullable = false)
+    private Timestamp creationDate;
 
-    @Column(name = "updateDate")
+    @Column(name = "updateDate", length = 100, nullable = false)
     private Timestamp updateDate;
 
 
-    public Role(int i, String n, String d){
-        id = i;
+    public Role(String n, String d){
         name = n;
         descripcion = d;
     }
 
-     */
+
 }
