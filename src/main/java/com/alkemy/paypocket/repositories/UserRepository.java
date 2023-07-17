dcd02b34b39cac8a9d252e31812a2eff7480de9b
Karen Lettieri <karulettieri2109@gmail.com>
2023-07-17 17:37:58 -0300

    entidad user y repository finalizados y testeados

---
+++ src/main/java/com/alkemy/paypocket/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.alkemy.paypocket.repositories;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.alkemy.paypocket.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    /* public User findByEmail(String email); */
+}
