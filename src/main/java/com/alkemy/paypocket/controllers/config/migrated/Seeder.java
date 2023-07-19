b76d4457b60734aa26fc5e700ffba7d6a604c7f3
Dante Giuliano <dante.giuliano@est.fi.uncoma.edu.ar>
2023-07-18 21:12:41 -0300

    seeder actualizado

--- src/main/java/com/alkemy/paypocket/controllers/config/migrated/Seeder.java
+++ src/main/java/com/alkemy/paypocket/controllers/config/migrated/Seeder.java
@@ -1,6 +1,5 @@
 package com.alkemy.paypocket.controllers.config.migrated;
 
-import java.security.Timestamp;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ public class Seeder {
 
     private final JdbcTemplate jdbcTemplate;
 
+
     @Autowired
     public Seeder(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -19,28 +19,51 @@ public class Seeder {
 
 
 
-    // private void usuario(int id_rol) {
-    //     String sql = "INSERT INTO users (name) VALUES (?)";
-    //     jdbcTemplate.update(sql, name);
-    // }
+    private void usuario(int id_rol) {
+        String now = LocalDateTime.now().toString();
+        String nombre   = Ramdomizer.nombre();
+        String apellido = Ramdomizer.apellido();
+        String email    = Ramdomizer.email(nombre, apellido);
+        String password = Ramdomizer.password();
+
+        String sql = "INSERT INTO user (creation_date, email, first_name, last_name, passwords, soft_delete, update_date, role_id) VALUES (";
+        sql+="'"+now+"','"+email+"','"+nombre+"','"+apellido+"','"+password+"',"+1+",'"+now+"',"+id_rol+")";
+
+
+        jdbcTemplate.update(sql);
+    }
 
 
     private void rol(String name, String descripcion) {
+
         String now = LocalDateTime.now().toString();
         String values = "VALUES('" + name + "','" + descripcion + "','" + now + "','" + now + "')";
         String sql = "INSERT INTO roles (role_name, role_desc, creation_date, update_date) " + values;
+
+      
         jdbcTemplate.update(sql);
     }
     
     public String migrate() {
         try {
             this.rol("Administrador", "Rol de Administrador");
+            this.rol("Regulares", "Rol Regular");
+
+
+            for (int i = 0; i < 100; i++) {
+                this.usuario(1);
+            }
+            for (int i = 0; i < 100; i++) {
+                this.usuario(2);
+            }
+
             return "MIGRACION SEEDER OK :)";
         } catch (Exception e) {
             return e.getMessage();
         }
     }
-    
+
+  
 
 
 }
