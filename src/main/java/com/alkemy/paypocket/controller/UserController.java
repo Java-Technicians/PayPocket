aa3266822785d8dd18caffe67e732e37dfc76e03
ignacioxherrera <ignacioherrera20212@gmail.com>
2023-07-19 12:56:02 -0300

    Se agrego el retorno del resonse.

--- src/main/java/com/alkemy/paypocket/controller/UserController.java
+++ src/main/java/com/alkemy/paypocket/controller/UserController.java
@@ -4,6 +4,7 @@ import com.alkemy.paypocket.entities.User;
 import com.alkemy.paypocket.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,8 @@ public class UserController {
     UserService userService;
 
     @GetMapping(path = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<User> findAllListarlos() {
-        return userService.findAll();
+    public ResponseEntity<List<User>> getUsers(){
+        List<User> userList = userService.findAll();
+        return ResponseEntity.ok(userList);
     }
 }
