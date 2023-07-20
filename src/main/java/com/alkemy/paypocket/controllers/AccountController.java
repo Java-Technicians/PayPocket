b5564536cf281d18bb11aa8df904d8b3e4ee0d03
Dante Giuliano <dante.giuliano@est.fi.uncoma.edu.ar>
2023-07-19 22:20:17 -0300

    limpieza de importaciones

--- src/main/java/com/alkemy/paypocket/controllers/AccountController.java
+++ src/main/java/com/alkemy/paypocket/controllers/AccountController.java
@@ -1,10 +1,8 @@
 package com.alkemy.paypocket.controllers;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/alkemy/paypocket/dtos/UserDto.java
+++ src/main/java/com/alkemy/paypocket/dtos/UserDto.java
@@ -1,7 +1,6 @@
 package com.alkemy.paypocket.dtos;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
