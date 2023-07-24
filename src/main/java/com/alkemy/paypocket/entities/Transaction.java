c0df26b4dc0599b7cb04fb35f137067d1ea0b0b4
Brainton <hbraiton@gmail.com>
2023-07-24 14:22:55 -0300

    Crear modelo y repositorio de Transactions OK

---
+++ src/main/java/com/alkemy/paypocket/entities/Transaction.java
@@ -0,0 +1,39 @@
+package com.alkemy.paypocket.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_Id")
+    private Integer id;
+
+    @Column(name = "amount", nullable = false)
+    private Double amount;
+
+    @Column(name = "type", nullable = false, length = 100)
+    private String type;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "transactionDate")
+    private LocalDate transactionDate;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+}
