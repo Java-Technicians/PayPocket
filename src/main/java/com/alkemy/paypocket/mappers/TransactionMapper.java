3cc0e4f3bf6153a8ff68f794486f551a0fb403fc
Brainton <hbraiton@gmail.com>
2023-07-24 21:09:13 -0300

    Endpoint para realizar un depósito

---
+++ src/main/java/com/alkemy/paypocket/mappers/TransactionMapper.java
@@ -0,0 +1,29 @@
+package com.alkemy.paypocket.mappers;
+
+import com.alkemy.paypocket.dtos.TransactionDto;
+import com.alkemy.paypocket.entities.Transaction;
+import com.alkemy.paypocket.repositories.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class TransactionMapper {
+
+    @Autowired
+    AccountRepository accountRepository;
+    public Transaction toTransaction(TransactionDto transactionDto){
+
+        Transaction newTransaction = new Transaction();
+
+        newTransaction.setAccount(accountRepository.findById(transactionDto.getAccountId()).get());
+        newTransaction.setType(transactionDto.getType());
+        newTransaction.setAmount(transactionDto.getAmount());
+        newTransaction.setTransactionDate(LocalDate.now());
+        newTransaction.setDescription("deposito");
+
+        return newTransaction;
+    }
+
+}
---
+++ src/main/java/com/alkemy/paypocket/services/TransactionService.java
@@ -0,0 +1,53 @@
+package com.alkemy.paypocket.services;
+
+import com.alkemy.paypocket.Message.ResponseData;
+import com.alkemy.paypocket.dtos.TransactionDto;
+import com.alkemy.paypocket.entities.Account;
+import com.alkemy.paypocket.entities.Transaction;
+import com.alkemy.paypocket.mappers.TransactionMapper;
+import com.alkemy.paypocket.repositories.AccountRepository;
+import com.alkemy.paypocket.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    TransactionRepository transactionRepository;
+
+    @Autowired
+    TransactionMapper transactionMapper;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    public ResponseData<Transaction> saveTransaction(TransactionDto transactionDto){
+
+        try {
+            transactionDto.setType("DEPOSITO"); /*Al ser solicitado por el EndPoint de deposito seteo el type en DEPOSITO*/
+
+            Transaction transaction = transactionMapper.toTransaction(transactionDto);
+
+            Account account = accountRepository.findById(transaction.getAccount().getId_account()).get();
+
+            account.setBalance(transaction.getAmount());
+
+            accountRepository.save(account);
+
+            transactionRepository.save(transaction);
+
+
+            return new ResponseData<>(transaction,"Transaccion Guardada");
+
+        }catch(Exception e){
+            e.printStackTrace(); // Imprime el error en la consola (opcional)
+            return new ResponseData<>(null,"Error al registrar transaccion");
+        }
+
+
+
+
+    }
+
+}
