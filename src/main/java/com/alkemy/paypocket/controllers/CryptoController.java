80c4e5892ed779c286e120f81a997255cf19bc5d
ignacioxherrera <ignacioherrera20212@gmail.com>
2023-08-03 21:13:55 -0300

    Agregado el controlador de obtener crypto.

--- src/main/java/com/alkemy/paypocket/controllers/CryptoController.java
+++ src/main/java/com/alkemy/paypocket/controllers/CryptoController.java
@@ -1,23 +1,30 @@
 package com.alkemy.paypocket.controllers;
 
 import com.alkemy.paypocket.dtos.CryptoDto;
+import com.alkemy.paypocket.repositories.CryptoRepository;
 import com.alkemy.paypocket.services.CryptoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "crypto")
+@Tag(name = "Crypto", description = "Controlador de Crypto.")
 public class CryptoController {
 
     @Autowired
     CryptoService cryptoService;
 
+    @Autowired
+    CryptoRepository cryptoRepository;
+
     @PostMapping(path = "/register")
+    @Operation(summary = "Crear", description = "Crea una crypto.")
     public ResponseEntity<?> registerCrypto(@RequestBody @Valid CryptoDto cryptoDto) {
 
         try {
@@ -26,6 +33,12 @@ public class CryptoController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping(path = "/{crypto_id}" )
+    @Operation(summary = "Obtener", description = "Obtiene una crypto por id.")
+    public ResponseEntity<?> getCrypto(@PathVariable("crypto_id") Integer id){
+        return ResponseEntity.ok(cryptoRepository.findById(id));
     }
 
 }
